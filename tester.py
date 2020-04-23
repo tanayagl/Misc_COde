414f38ea7a75331b02641678bbaa0db458c154a1
Tanay Agrawal <Tanay@Tanays-MacBook-Pro.local>
2020-04-24 00:23:53 +0530

    Running code with Question Defecit. Optimised for multiple test. Limit Not checked

--- tester.py
+++ tester.py
@@ -22,7 +22,6 @@ def check_limits(topic_floor,topic_decimal,dict_topic_limits):
                 print("running")
                 topic_floor[key]=dict_topic_limits[key]["Upper_limit"]
                 topic_decimal[key]=0.00
-        
     return topic_floor,topic_decimal
 
 def get_question_bank(path):
@@ -35,96 +34,116 @@ def get_question_bank(path):
         array_question_bank.append({"QID": columns[0].strip(),"assessment":columns[1].strip(),"topic":columns[2].strip('[""""]'),"tag":columns[3].strip()})        
     return array_question_bank
 
-def get_topic_count(array_question_bank,current_assesment):    
-    dict_topic_count = {}
-    for question in array_question_bank:    
-            topic = question["topic"]
-            qb_assesment = question["assessment"]
-            if(qb_assesment == current_assesment):
-                if topic in dict_topic_count :
-                    dict_topic_count[topic] = dict_topic_count[topic] + 1   
-                else:
-                    dict_topic_count[topic] = 1
-    return dict_topic_count
+def get_topic_count(array_question_bank,input_question_distribution):    
+    dict_assessment_topic_count = {}
+    for assessment in input_question_distribution.keys():
+        dict_topic_count = {}
+        for question in array_question_bank:    
+                topic = question["topic"]
+                qb_assesment = question["assessment"]
+                if(qb_assesment == assessment):
+                    if topic in dict_topic_count :
+                        dict_topic_count[topic] = dict_topic_count[topic] + 1   
+                    else:
+                        dict_topic_count[topic] = 1
+        dict_assessment_topic_count[assessment] = dict_topic_count
+    return dict_assessment_topic_count
     
 # extract question, calc ratio, total ques
-def stats(dict_topic_count,total_questions,dict_topic_decimal):
+def stats(dict_topic_count,total_questions):
+    topic_decimal = {}
+    topic_floor = dict_topic_count.copy()
     total_topic_question = 0
-    # dict_topic_decimal={}
     for i in dict_topic_count: 
         total_topic_question = total_topic_question + dict_topic_count[i]
-    # print (sum)
-    ratio = 0.0
+    ratio = 0.00
     for i in dict_topic_count:
         ratio = dict_topic_count[i]*total_questions/total_topic_question
-        ceiling= math.ceil(ratio)
-        decimal_value=round((ratio*10)%10)/10
+        decimal_value = float(str(ratio-int(ratio))[1:])
         floor = math.floor(ratio)
-        dict_topic_count[i] = floor
-        if i in dict_topic_decimal:
-            dict_topic_decimal[i] = decimal_value + dict_topic_decimal[i]
-        else:
-            dict_topic_decimal[i] = decimal_value
-    return dict_topic_count,dict_topic_decimal
+        topic_floor[i] = floor
+        topic_decimal[i] = decimal_value
+    return topic_floor,topic_decimal
 
 def print_dict(dict):
     for i in dict:
         print(i,dict[i])
     print("\n")
 
+def update_decimal_bucket(topic_decimal,assessment):
+    if bool(decimal_bucket[assessment]):
+        for topic in (decimal_bucket[assessment]):
+            decimal_bucket[assessment][topic] = topic_decimal[topic] + decimal_bucket[assessment][topic]
+    else:
+        decimal_bucket[assessment] = topic_decimal.copy()
+    return decimal_bucket
 
-def brahmastra(topic_decimal, topic_floor):
-    ques_sum=0
+def brahmastra(topic_decimal, topic_floor,assessment,total_questions):
+    decimal_bucket = update_decimal_bucket(topic_decimal,assessment) 
+    topic_question_distribution = topic_floor.copy()
+    ques_sum = 0
     for i in topic_floor:
         ques_sum = topic_floor[i]+ques_sum
     remaining_question = total_questions-ques_sum
     count = 0
-    # dict_sorted = {value:key for key, value in topic_decimal.items()}
-
+    
     for i in range(0,remaining_question):
-        # if count == remaining_question:
-        #     break
-        topic = max(topic_decimal, key=topic_decimal.get)
-        topic_floor[topic] = topic_floor[topic] + 1 
-        topic_decimal[topic] = 0 if topic_floor[topic] - 1 < 0 else topic_floor[topic] - 1 
+        topic = max(decimal_bucket[assessment], key=decimal_bucket[assessment].get)
+        topic_question_distribution[topic] = topic_question_distribution[topic] + 1 
+        decimal_bucket[assessment][topic] = 0 if topic_decimal[topic] - 1 < 0 else topic_decimal[topic] - 1 
         count+=1
-    return topic_decimal,topic_floor
 
-def export_question(fw,test_count,question_bank,topic_question_distribution):
+    return topic_question_distribution
+def export_question(fw,topic_question_distribution):
     for topic in topic_question_distribution:
         question_count = topic_question_distribution[topic] 
         for i in range(0,question_count):
+            question_found = False
             for question in question_bank:    
                 if(question["topic"]==topic):
+                    question_found = True
                     fw.write(str(question["QID"])+","+str(topic)+"\n")
                     print(str(question["QID"])+","+str(topic)+"\n")
                     question_bank.remove(question)
                     topic_question_distribution[topic] = topic_question_distribution[topic] - 1    
-                    break
+                    break       
+            if(question_found==False):
+                return -1
+    return 0        
                 
-
-    
-
-
 assessment_areas = ["Logical Reasoning/ Data Interpretation","Verbal Ability","Quantitative Aptitude"]
 # question_bank = get_question_bank("C:/Users/hp/Desktop/MyCodes/python codes/test_maker/QB.csv")
-topic_limits=get_limits("C:/Users/hp/Desktop/MyCodes/python codes/test_maker/limitss.csv")
-question_bank = get_question_bank("QB.csv")
+# topic_limits=get_limits("C:/Users/hp/Desktop/MyCodes/python codes/test_maker/limits.csv")
+
+# topic_limits = get_limits
 input_question_distribution = { assessment_areas[0]: 15, assessment_areas[1] : 30,assessment_areas[2]: 20}
-decimal_bucket={assessment_areas[0]:{}, assessment_areas[1]:{ }, assessment_areas[2]:{},}
-floor={}
+status = -1
 test_count = 0
 fw = open("Export"+str(time.time()),"w")
-fw.write(str("Test")+str(test_count)+"\n")
-for assessment in input_question_distribution :
-    test_count+=1
-    total_questions = input_question_distribution[assessment]
-    topic_floor = get_topic_count(question_bank,assessment)
-    topic_question_distribution,decimal_bucket[assessment] = stats(topic_floor,total_questions,decimal_bucket[assessment])
-    check_limits(topic_floor,topic_limits)
-    decimal_bucket[assessment],topic_question_distribution = brahmastra(decimal_bucket[assessment],topic_question_distribution)
-    export_question(fw,test_count,question_bank,topic_question_distribution)
-    print_dict(decimal_bucket)
-    print_dict(topic_question_distribution)
+topic_question_distribution = {}
+topic_floor = {}
+topic_decimal = {}
+decimal_bucket = {}
+question_bank = get_question_bank("QB.csv")
+dict_assessment_topic_count = get_topic_count(question_bank,input_question_distribution)
 
+for assessment in input_question_distribution :
+    decimal_bucket[assessment] = {}
+    total_questions = input_question_distribution[assessment]
+    topic_floor[assessment],topic_decimal[assessment] = stats(dict_assessment_topic_count[assessment],total_questions)
+    
+while(1==1):
+    fw.write(str("Test")+str(test_count)+"\n")
+    test_count+=1
+    for assessment in input_question_distribution:
+        total_questions = input_question_distribution[assessment]
+        topic_question_distribution[assessment] = brahmastra(topic_decimal[assessment],topic_floor[assessment],assessment,total_questions)
+        status = export_question(fw,topic_question_distribution[assessment])
+        # print_dict(decimal_bucket)
+        # print_dict(topic_question_distribution)
+        if status==-1:
+            break
+    if(status ==-1):
+        print("Test Distributed. Test Excel")
+        break
 fw.close()
