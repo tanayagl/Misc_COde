fb59f18e19ce8cd9affd536bdb8d4a56ab0a524c
Tanay Agrawal <Tanay@Tanays-MacBook-Pro.local>
2020-04-24 18:11:05 +0530

    Subtopic Specific Distribution running. Distribution Double Check

--- tester.py
+++ tester.py
@@ -31,38 +31,39 @@ def get_question_bank(path):
     lines = excel.readlines()
     for line in lines:     
         columns = line.split(',')
-        array_question_bank.append({"QID": columns[0].strip(),"assessment":columns[1].strip(),"topic":columns[2].strip('[""""]'),"tag":columns[3].strip()})        
+        array_question_bank.append({"QID": columns[0].strip(),"assessment":columns[1].strip(),"topic":columns[2].strip('[""""]'),"tag":columns[3].strip().strip('[""""]')})        
     return array_question_bank
 
-def get_topic_count(array_question_bank,input_question_distribution):    
-    dict_assessment_topic_count = {}
-    for assessment in input_question_distribution.keys():
-        dict_topic_count = {}
+# Get child count, for the list of each parent from the question
+def get_child_count(array_question_bank,parent_list,qb_parent_header,qb_child_header):    
+    dict_parent_child_count = {}
+    for parent in parent_list:
+        dict_child_count = {}
         for question in array_question_bank:    
-                topic = question["topic"]
-                qb_assesment = question["assessment"]
-                if(qb_assesment == assessment):
-                    if topic in dict_topic_count :
-                        dict_topic_count[topic] = dict_topic_count[topic] + 1   
-                    else:
-                        dict_topic_count[topic] = 1
-        dict_assessment_topic_count[assessment] = dict_topic_count
-    return dict_assessment_topic_count
+            qb_parent = question[qb_parent_header]
+            qb_child = question[qb_child_header]
+            if(qb_parent == parent):
+                if qb_child in dict_child_count :
+                    dict_child_count[qb_child] = dict_child_count[qb_child] + 1   
+                else:
+                    dict_child_count[qb_child] = 1
+        dict_parent_child_count[parent] = dict_child_count
+    return dict_parent_child_count
     
 # extract question, calc ratio, total ques
 def stats(dict_topic_count,total_questions):
     topic_decimal = {}
     topic_floor = dict_topic_count.copy()
     total_topic_question = 0
-    for i in dict_topic_count: 
-        total_topic_question = total_topic_question + dict_topic_count[i]
+    for topic in dict_topic_count: 
+        total_topic_question = total_topic_question + dict_topic_count[topic]
     ratio = 0.00
-    for i in dict_topic_count:
-        ratio = dict_topic_count[i]*total_questions/total_topic_question
+    for topic in dict_topic_count:
+        ratio = 0.00 if total_questions==0 else dict_topic_count[topic]*total_questions/total_topic_question
         decimal_value = float(str(ratio-int(ratio))[1:])
         floor = math.floor(ratio)
-        topic_floor[i] = floor
-        topic_decimal[i] = decimal_value
+        topic_floor[topic] = floor
+        topic_decimal[topic] = decimal_value
     return topic_floor,topic_decimal
 
 def print_dict(dict):
@@ -71,6 +72,8 @@ def print_dict(dict):
     print("\n")
 
 def update_decimal_bucket(topic_decimal,assessment):
+    if(assessment not in decimal_bucket):
+        decimal_bucket[assessment] = {}
     if bool(decimal_bucket[assessment]):
         for topic in (decimal_bucket[assessment]):
             decimal_bucket[assessment][topic] = topic_decimal[topic] + decimal_bucket[assessment][topic]
@@ -86,26 +89,25 @@ def brahmastra(topic_decimal, topic_floor,assessment,total_questions):
         ques_sum = topic_floor[i]+ques_sum
     remaining_question = total_questions-ques_sum
     count = 0
-    
     for i in range(0,remaining_question):
         topic = max(decimal_bucket[assessment], key=decimal_bucket[assessment].get)
         topic_question_distribution[topic] = topic_question_distribution[topic] + 1 
         decimal_bucket[assessment][topic] = 0 if topic_decimal[topic] - 1 < 0 else topic_decimal[topic] - 1 
         count+=1
-
     return topic_question_distribution
-def export_question(fw,topic_question_distribution):
-    for topic in topic_question_distribution:
-        question_count = topic_question_distribution[topic] 
+    
+def export_question(fw,question_distribution,qb_header):
+    for topic in question_distribution:
+        question_count = question_distribution[topic] 
         for i in range(0,question_count):
             question_found = False
             for question in question_bank:    
-                if(question["topic"]==topic):
+                if(question[qb_header]==topic):
                     question_found = True
                     fw.write(str(question["QID"])+","+str(topic)+"\n")
                     print(str(question["QID"])+","+str(topic)+"\n")
                     question_bank.remove(question)
-                    topic_question_distribution[topic] = topic_question_distribution[topic] - 1    
+                    question_distribution[topic] = question_distribution[topic] - 1    
                     break       
             if(question_found==False):
                 return -1
@@ -124,21 +126,28 @@ topic_question_distribution = {}
 topic_floor = {}
 topic_decimal = {}
 decimal_bucket = {}
-question_bank = get_question_bank("QB.csv")
-dict_assessment_topic_count = get_topic_count(question_bank,input_question_distribution)
+st_floor = {}
+st_decimal = {}
+dict_topic_st_count = {}
+st_question_distribution = {}
+dict_parent_child_count = {}
 
+question_bank = get_question_bank("QB.csv")
+dict_assessment_topic_count = get_child_count(question_bank,input_question_distribution.keys(),"assessment","topic")
 for assessment in input_question_distribution :
-    decimal_bucket[assessment] = {}
+    dict_topic_st_count[assessment] = get_child_count(question_bank,dict_assessment_topic_count[assessment].keys(),"topic","tag")
     total_questions = input_question_distribution[assessment]
     topic_floor[assessment],topic_decimal[assessment] = stats(dict_assessment_topic_count[assessment],total_questions)
-    
 while(1==1):
     fw.write(str("Test")+str(test_count)+"\n")
     test_count+=1
     for assessment in input_question_distribution:
         total_questions = input_question_distribution[assessment]
         topic_question_distribution[assessment] = brahmastra(topic_decimal[assessment],topic_floor[assessment],assessment,total_questions)
-        status = export_question(fw,topic_question_distribution[assessment])
+        for topic in topic_question_distribution[assessment]:
+            st_floor[topic],st_decimal[topic] = stats(dict_topic_st_count[assessment][topic],topic_question_distribution[assessment][topic])
+            st_question_distribution[topic] = brahmastra(st_decimal[topic],st_floor[topic],topic,topic_question_distribution[assessment][topic])
+            status = export_question(fw,st_question_distribution[topic],"tag")
         # print_dict(decimal_bucket)
         # print_dict(topic_question_distribution)
         if status==-1:
